449b13b8dbbec881dff499bf789a04320afb025f
at670475 <andrea.tabone@broadcom.com>
2020-07-01 17:32:56 +0200

    Fix bug

--- src/main/java/com/restservice/restfulwebservices/configuration/SwaggerConfig.java
+++ src/main/java/com/restservice/restfulwebservices/configuration/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.restservice.restfulwebservices.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -10,15 +11,19 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    @Value("${apiml.service.title}")
+    private String apiTitle;
 
+    @Value("${apiml.service.apiInfo[0].version}")
+    private String apiVersion;
+
+    @Value("${apiml.service.description}")
+    private String apiDescription;
     public static final Contact DEFAULT_CONTACT = new Contact("Andrea Tabone", "http:blabla", "andrea.tabone89@gmail.com");
     private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awesome Api Title", "Awesome Api Documentation", "1.0", "urn:tos", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList());
     private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(Arrays.asList("application/json", "application/xml"));
@@ -30,9 +35,18 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/api/v1/**"))
                 .build()
-                .apiInfo(DEFAULT_API_INFO)
-                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
-                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+                .apiInfo(new ApiInfo(
+                        apiTitle,
+                        apiDescription,
+                        apiVersion,
+                        null,
+                        null,
+                        null,
+                        null,
+                        Collections.emptyList()
+                ));
+//                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+//                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 
 }
